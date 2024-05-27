734b40f6aa7e45b828d095c9beb29bdaa02c38d6
Pedro Miguel Marques Rodrigues <pedrommarquesrodrigues@gmail.com>
2024-05-27 17:30:25 +0100

    Added validation functions to validate forms and add styles when form is empty when submit

---
+++ funcoes.js
@@ -0,0 +1,45 @@
+const validacaoForm = () => {
+  const username = document.getElementById("utilizador");
+  const pass = document.getElementById("password");
+
+  // Verifica se campos estão vazios
+  if (username.value === "") {
+    username.classList.add("input-error");
+    setError(username, "Este campo não pode estar vazio.");
+    return false;
+  } else {
+    username.classList.remove("input-error");
+    setError(username, "");
+  }
+
+  if (pass.value === "") {
+    pass.classList.add("input-error");
+    setError(pass, "Escreva a sua palavra-passe.");
+    return false;
+  } else {
+    password.classList.remove("input-error");
+    setError(pass, "");
+  }
+  return true;
+};
+
+// Função que vai buscar o elemento e adiciona o texto de erro
+const setError = (element, message) => {
+  const inputControl = element.parentElement; // vou buscar o 'pai' do que lhe passo como element
+  const errorDisplay = inputControl.querySelector(".error"); // vou ao 'pai' buscar o que tem a classe de error
+  errorDisplay.innerText = message;
+  element.focus();
+};
+
+//Func to force the label to be on top
+
+const inputActive = (target, labelID) => {
+  const label = document.getElementById(labelID);
+  const inputName = document.getElementById(target);
+
+  if (inputName.value !== "") {
+    label.classList.add("force_top_label");
+  } else {
+    label.classList.remove("force_top_label");
+  }
+};
